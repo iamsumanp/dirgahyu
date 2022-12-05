86662bb134bf7e5eaa1dbb6ac258cfe47cb9a71d
suman pokharel <sumanpok9@gmail.com>
2022-12-06 02:32:18 +0545

    responsive nav bar, hamburger menu added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,52 +4,83 @@ import Image from "next/image";
 import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
 import { FaHandshake } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
+import { IoIosClose } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 import { MdLogin } from "react-icons/md";
 import Link from "next/link";
 export const NavBar = () => {
+  const [click, setClick] = React.useState(false);
+
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
   return (
-    <div className={styles.navBar}>
-      <Image
-        src="https://res.cloudinary.com/hire-station/image/upload/v1668150540/dirgahyu/dirgahyu-small_c7dbos.png"
-        alt="Galaxy"
-        height={1000}
-        width={1000}
-        className={styles.logoImg}
-      />
-      <div className={styles.navSection}>
-        <ul className={styles.nav_ul}>
-          <li>
-            <Link href="/" className={styles.listItem}>
-              <AiFillHome className={styles.icon} /> Home
-            </Link>
-          </li>
-          <li className={styles.listItem}>
-            <Link href="/register" style={{ textDecoration: "none" }}>
-              <FaHandshake className={styles.icon} />
-              Join Us
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className={styles.listItem}>
-              <AiFillInfoCircle className={styles.icon} /> About Us
-            </Link>
-          </li>
-          <li className={styles.listItem}>
-            <Link href="/volunteerList" style={{ textDecoration: "none" }}>
-              <BsFillPeopleFill className={styles.icon} /> Volunteers
-            </Link>
-          </li>
-        </ul>
-        <div className={styles.loginbtndiv}>
-          <MdLogin className={styles.icon} size={20} />
-          <Link href="/login" className={styles.listItem}>
-            <span className={styles.LogInBtn}>
-              {/* <Link href="/about" className={styles.listItem}> */}
-              LogIn
-              {/* </Link> */}
-            </span>
-          </Link>
-        </div>
+    <div className={styles.test}>
+      <div className={styles.main_container} onClick={() => Close()}>
+        <nav className={styles.navBar} onClick={(e) => e.stopPropagation()}>
+          <Image
+            src="https://res.cloudinary.com/hire-station/image/upload/v1668150540/dirgahyu/dirgahyu-small_c7dbos.png"
+            alt="Galaxy"
+            height={1000}
+            width={1000}
+            className={styles.logoImg}
+          />
+          <div className={styles.navSection}>
+            <ul className={click ? styles.nav_ul_active : styles.nav_ul}>
+              <li className={styles.listItem}>
+                <Link
+                  href="/"
+                  className={styles.nextLink}
+                  onClick={handleClick}
+                >
+                  <AiFillHome className={styles.icon} />
+                  Home
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link href="/register" className={styles.nextLink}>
+                  <FaHandshake className={styles.icon} />
+                  Join Us
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link href="/about" className={styles.nextLink}>
+                  <AiFillInfoCircle className={styles.icon} />
+                  About Us
+                </Link>
+              </li>
+              <li className={styles.listItem}>
+                <Link
+                  href="/volunteerList"
+                  style={{ textDecoration: "none" }}
+                  className={styles.nextLink}
+                >
+                  <BsFillPeopleFill className={styles.icon} />
+                  Volunteers
+                </Link>
+              </li>
+            </ul>
+            <div className={styles.login_hamburger}>
+              <div className={styles.nav_icon} onClick={handleClick}>
+                {click ? (
+                  <IoMdClose size={35} />
+                ) : (
+                  <GiHamburgerMenu size={30} />
+                )}
+              </div>
+              <div className={styles.loginbtndiv}>
+                <Link href="/login" className={styles.nextLink}>
+                  <MdLogin className={styles.icon} size={20} />
+                  <span className={styles.LogInBtn}>
+                    {/* <Link href="/about" className={styles.listItem}> */}
+                    LogIn
+                    {/* </Link> */}
+                  </span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   );
